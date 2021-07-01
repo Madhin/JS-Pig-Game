343fefe4dd517bf848f5767507050679a26365e1
Madhin <madhinhussain@gmail.com>
2021-07-01 15:18:30 +0100

    adding in JS functionality

---
+++ app.dev.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var score0 = document.querySelector("#score--0");
+var score1 = document.querySelector("#score--1");
+var dice = document.querySelector(".dice");
+score0.textContent = 0;
+score1.textContent = 0;
+dice.classList.add("hidden");
