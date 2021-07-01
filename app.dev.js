056cd00d5047f78dcb57bc33275678fe97fcf8fa
Madhin <madhinhussain@gmail.com>
2021-07-01 16:39:05 +0100

    added in dice functionality

--- app.dev.js
+++ app.dev.js
@@ -1,8 +1,27 @@
 "use strict";
 
-var score0 = document.querySelector("#score--0");
-var score1 = document.querySelector("#score--1");
+var score0 = document.querySelector("#score__0");
+var score1 = document.querySelector("#score__1");
 var dice = document.querySelector(".dice");
+var btnNew = document.querySelector(".btn__new");
+var btnRoll = document.querySelector(".btn__roll");
+var btnHold1 = document.querySelector(".btn__hold1");
+var btnHold2 = document.querySelector(".btn__hold2");
+var current0 = document.querySelector("#current__0");
+var current1 = document.querySelector("#current__1");
 score0.textContent = 0;
 score1.textContent = 0;
 dice.classList.add("hidden");
+var currentScore = 0; //roll dice function
+
+btnRoll.addEventListener("click", function () {
+  var generateRoll = Math.trunc(Math.random() * 6) + 1;
+  console.log(generateRoll);
+  dice.classList.remove("hidden");
+  dice.src = "dice-".concat(generateRoll, ".png");
+
+  if (generateRoll !== 1) {
+    currentScore += generateRoll;
+    current0.textContent = currentScore;
+  }
+});
--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-const score0 = document.querySelector("#score--0");
-const score1 = document.querySelector("#score--1");
+const score0 = document.querySelector("#score__0");
+const score1 = document.querySelector("#score__1");
 const dice = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn__new");
+const btnRoll = document.querySelector(".btn__roll");
+const btnHold1 = document.querySelector(".btn__hold1");
+const btnHold2 = document.querySelector(".btn__hold2");
+const current0 = document.querySelector("#current__0");
+const current1 = document.querySelector("#current__1");
 
 score0.textContent = 0;
 score1.textContent = 0;
 dice.classList.add("hidden");
+
+let currentScore = 0;
+
+//roll dice function
+btnRoll.addEventListener("click", () => {
+  const generateRoll = Math.trunc(Math.random() * 6) + 1;
+  console.log(generateRoll);
+  dice.classList.remove("hidden");
+  dice.src = `dice-${generateRoll}.png`;
+  if (generateRoll !== 1) {
+    currentScore += generateRoll;
+    current0.textContent = currentScore;
+  }
+});
